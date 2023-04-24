a8a65c171d543367bddd56b05cd130f507ee7440
Prashant Prithyani <prithyani2002@gmail.com>
2023-04-24 19:01:26 +0530

    .

--- routes/nfts.js
+++ routes/nfts.js
@@ -33,86 +33,49 @@ router.post("/mint", async (req, res) => {
 router.get("/mint", async (req, res) => {
   const query = req.query;
   console.log(query);
+  queryObject={};
 
   try {
-    const nfts = await nftDetails.find( { 
-      $or: [
-        {},
-        { _id: query.id },
-        { owner: query.owner },
-        { tokenAddress: query.tokenAddress },
-        { isOnSale: query.isOnSale },
-        { price: query.price },
-        { timestamp: query.timestamp },
-        {tokenID: query.tokenId}
-      ]
-    } );
-    
-    console.log(nfts);
-    res.status(200).json({ nfts });
-    // if (query.id) {
-    //   try {
-    //     const nfts = await nftDetails.find({ _id: query.id });
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else if (query.owner) {
-    //   try {
-    //     const nfts = await nftDetails.find({ owner: query.owner });
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else if (query.tokenAddress) {
-    //   try {
-    //     const nfts = await nftDetails.find({
-    //       tokenAddress: query.tokenAddress,
-    //     });
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else if (query.isOnSale) {
-    //   try {
-    //     const nfts = await nftDetails.find({ isOnSale: query.isOnSale });
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else if (query.price) {
-    //   try {
-    //     const nfts = await nftDetails.find({ price: query.price });
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else if (query.timestamp) {
-    //   try {
-    //     const nfts = await nftDetails.find({ timestamp: query.timestamp });
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else if (query.tokenId) {
-    //   try {
-    //     const nfts = await nftDetails.find({ tokenID: query.tokenId });
-    //     console.log(nfts);
-    //     res.status(200).json({ nfts });
-    //   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Internal server error" });
-    //   }
-    // } else {
-    //   // const nfts = await nftDetails.find();
-    //   res.status(200).json("nfts");
-    // }
+    // const nfts = await nftDetails.find( {
+    //   $or: [
+    //     {},
+    //     { _id: query.id },
+    //     { owner: query.owner },
+    //     { tokenAddress: query.tokenAddress },
+    //     { isOnSale: query.isOnSale },
+    //     { price: query.price },
+    //     { timestamp: query.timestamp },
+    //     {tokenID: query.tokenId}
+    //   ]
+    // } );
+
+    // console.log(nfts);
+    // res.status(200).json({ nfts });
+    if (query.id) {
+      queryObject._id = query.id;
+    }
+    if (query.owner) {
+      queryObject.owner = query.owner;
+    }
+    if (query.tokenAddress) {
+      queryObject.tokenAddress = query.tokenAddress;
+    }
+    if (query.isOnSale) {
+      queryObject.isOnSale = query.isOnSale;
+    }
+    if (query.price) {
+      queryObject.price = query.price;
+    }
+    if (query.timestamp) {
+      queryObject.timestamp = query.timestamp;
+    }
+    if (query.tokenId) {
+      queryObject.tokenID = query.tokenId;
+    }
+    {
+      const nfts = await nftDetails.find(queryObject);
+      res.status(200).json(nfts);
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Internal server error" });
