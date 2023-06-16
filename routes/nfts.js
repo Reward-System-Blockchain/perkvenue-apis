cf07a77113fa1a6981338ae737314d0874b10e0a
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-06-17 02:18:57 +0530

    update swagger nft docs

--- routes/nfts.js
+++ routes/nfts.js
@@ -192,6 +192,83 @@ router.get("/details", async (req, res) => {
 
 // POST - QuickMint NFT
 
+/**
+ * @swagger
+ * /nfts/quickmint:
+ *   post:
+ *     summary: Create and mint new non-fungible tokens (NFTs) quickly.
+ *     tags:
+ *       - NFTs
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/QuickMintRequest'
+ *     responses:
+ *       200:
+ *         description: Success. Returns the details of the created and minted NFT.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/QuickMintResponse'
+ *       500:
+ *         description: Internal Server Error. An error occurred while processing the request.
+ */
+
+// Define the request body schema for quick minting
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     QuickMintRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the NFT.
+ *         description:
+ *           type: string
+ *           description: The description of the NFT.
+ *         image:
+ *           type: string
+ *           format: uri
+ *           description: The URL of the image for the NFT.
+ *         addressTo:
+ *           type: string
+ *           description: The address of the recipient of the NFT.
+ *       required:
+ *         - name
+ *         - description
+ *         - image
+ *         - addressTo
+ */
+
+// Define the response schema for quick minting
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     QuickMintResponse:
+ *       type: object
+ *       properties:
+ *         tokenId:
+ *           type: string
+ *           description: The token ID of the minted NFT.
+ *         contractAddress:
+ *           type: string
+ *           description: The address of the NFT contract.
+ *         owner:
+ *           type: string
+ *           description: The address of the NFT owner.
+ *         tokenURI:
+ *           type: string
+ *           description: The URI of the minted NFT.
+ *         txHash:
+ *           type: string
+ *           description: The hash value of the transaction related to the minted NFT.
+ */
+
 router.post("/quickmint", async (req, res) => {
   const { name, description, image, addressTo } = req.body;
 
@@ -205,36 +282,98 @@ router.post("/quickmint", async (req, res) => {
 });
 
 
+// Get collection details
 
+/**
+ * @swagger
+ * /nfts/collectionDetails:
+ *   get:
+ *     summary: Retrieve information about a deployed NFT collection.
+ *     tags:
+ *       - NFTs
+ *     parameters:
+ *       - in: query
+ *         name: contractAddress
+ *         schema:
+ *           type: string
+ *         description: The address of the NFT collection contract.
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: The name of the NFT collection.
+ *       - in: query
+ *         name: symbol
+ *         schema:
+ *           type: string
+ *         description: The symbol of the NFT collection.
+ *       - in: query
+ *         name: sort
+ *         schema:
+ *           type: string
+ *         description: The sorting criteria for the collection details (e.g., "name", "-timestamp").
+ *       - in: query
+ *         name: select
+ *         schema:
+ *           type: string
+ *         description: The fields to include in the collection details (e.g., "name,symbol,maxSupply").
+ *     responses:
+ *       200:
+ *         description: Success. Returns the details of the deployed NFT collection.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CollectionDetailsResponse'
+ *       500:
+ *         description: Internal Server Error. An error occurred while processing the request.
+ */
 
-//POST
-router.post("/mintFromCollection", async (req, res) => {
-  const { contractAddress, name, description, image, addressTo } = req.body;
+// Define the response schema for collection details
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CollectionDetailsResponse:
+ *       type: object
+ *       properties:
+ *         total:
+ *           type: integer
+ *           description: The total number of matching collections.
+ *         collections:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/NFTCollection'
+ */
 
-  try {
-    const nftDetails = await mintFromCollection(contractAddress, name, description, image, addressTo);
-    res.status(200).json({ nftDetails });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+// Define the schema for the NFT collection details
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     NFTCollection:
+ *       type: object
+ *       properties:
+ *         contractAddress:
+ *           type: string
+ *           description: The address of the NFT collection contract.
+ *         name:
+ *           type: string
+ *           description: The name of the NFT collection.
+ *         symbol:
+ *           type: string
+ *           description: The symbol of the NFT collection.
+ *         maxSupply:
+ *           type: number
+ *           description: The maximum supply of the NFT collection.
+ *         txHash:
+ *           type: string
+ *           description: The hash value of the transaction related to the deployment of the NFT collection.
+ *         timestamp:
+ *           type: string
+ *           format: date-time
+ *           description: The timestamp when the NFT collection was deployed.
+ */
 
-
-// POST
-router.post("/deploycollection", async (req, res) => {
-  const { name, symbol, maxSupply } = req.body;
-
-  try {
-    const contractAddress = await deployNFTCollection(name, symbol, maxSupply);
-    res.status(200).json({ contractAddress });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-// get collection details
 router.get("/collectionDetails", async (req, res) => {
   const { contractAddress, name, symbol, sort, select} =
     req.query;
@@ -265,11 +404,168 @@ router.get("/collectionDetails", async (req, res) => {
   res.status(200).json({ total: collections.length, collections });
 });
 
-//---------------------------------------------------------//
+
+// POST - Deploy Collection
+
+/**
+ * @swagger
+ * /nfts/deploycollection:
+ *   post:
+ *     summary: Deploy an NFT collection on the blockchain.
+ *     tags:
+ *       - NFTs
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DeployCollectionRequest'
+ *     responses:
+ *       200:
+ *         description: Success. Returns the address of the deployed NFT collection contract.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DeployCollectionResponse'
+ *       500:
+ *         description: Internal Server Error. An error occurred while processing the request.
+ */
+
+// Define the request schema for deploying an NFT collection
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     DeployCollectionRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the NFT collection.
+ *         symbol:
+ *           type: string
+ *           description: The symbol of the NFT collection.
+ *         maxSupply:
+ *           type: number
+ *           description: The maximum supply of the NFT collection.
+ */
+
+// Define the response schema for deploying an NFT collection
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     DeployCollectionResponse:
+ *       type: object
+ *       properties:
+ *         contractAddress:
+ *           type: string
+ *           description: The address of the deployed NFT collection contract.
+ */
+
+router.post("/deploycollection", async (req, res) => {
+  const { name, symbol, maxSupply } = req.body;
+
+  try {
+    const contractAddress = await deployNFTCollection(name, symbol, maxSupply);
+    res.status(200).json({ contractAddress });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 
 
 
 
+//POST - Mint From Collection
+
+/**
+ * @swagger
+ * /nfts/mintFromCollection:
+ *   post:
+ *     summary: Mint new NFTs from an existing NFT collection.
+ *     tags:
+ *       - NFTs
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MintFromCollectionRequest'
+ *     responses:
+ *       200:
+ *         description: Success. Returns the details of the minted NFTs.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MintFromCollectionResponse'
+ *       500:
+ *         description: Internal Server Error. An error occurred while processing the request.
+ */
+
+// Define the request schema for minting NFTs from a collection
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     MintFromCollectionRequest:
+ *       type: object
+ *       properties:
+ *         contractAddress:
+ *           type: string
+ *           description: The address of the NFT collection contract.
+ *         name:
+ *           type: string
+ *           description: The name of the NFT.
+ *         description:
+ *           type: string
+ *           description: The description of the NFT.
+ *         image:
+ *           type: string
+ *           description: The image URL of the NFT.
+ *         addressTo:
+ *           type: string
+ *           description: The address to which the minted NFT will be assigned.
+ */
+
+// Define the response schema for minting NFTs from a collection
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     MintFromCollectionResponse:
+ *       type: object
+ *       properties:
+ *         tokenId:
+ *           type: string
+ *           description: The ID of the minted NFT.
+ *         contractAddress:
+ *           type: string
+ *           description: The address of the NFT collection contract.
+ *         owner:
+ *           type: string
+ *           description: The address of the NFT owner.
+ *         tokenURI:
+ *           type: string
+ *           description: The URI of the minted NFT.
+ *         txHash:
+ *           type: string
+ *           description: The transaction hash of the minting transaction.
+ */
+
+
+router.post("/mintFromCollection", async (req, res) => {
+  const { contractAddress, name, description, image, addressTo } = req.body;
+
+  try {
+    const nftDetails = await mintFromCollection(contractAddress, name, description, image, addressTo);
+    res.status(200).json({ nftDetails });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
